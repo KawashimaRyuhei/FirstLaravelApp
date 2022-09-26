91641943eae6de8691d5a65198a40fd916e1fa57
kawashima <11011011wxy@gmail.com>
2022-09-26 23:48:03 +0900

    apd IndexController

--- app/Http/Controllers/Tweet/IndexController.php
+++ app/Http/Controllers/Tweet/IndexController.php
@@ -3,18 +3,22 @@
 namespace App\Http\Controllers\Tweet;
 
 use App\Http\Controllers\Controller;
+use App\models\Tweet;
 use Illuminate\Http\Request;
 
 class IndexController extends Controller
 {
     /**
-     * Handle the incoming request.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function __invoke(Request $request)
     {
-        return view('tweet.index', ['name' => 'laravel']);
+        $tweets = Tweet::all();
+        dd($tweets);
+        return view('tweet.index')
+        ->with('name', 'laravel')
+        ->with('version', '8');
     }
 }
