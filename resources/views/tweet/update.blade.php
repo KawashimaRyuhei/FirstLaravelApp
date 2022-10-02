b75f759725fadfd2ea4d00985c4c1b9e8502a384
kawashima <11011011wxy@gmail.com>
2022-10-02 20:01:09 +0900

    apd PutController2

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -9,8 +9,11 @@
     <body>
         <h1>つぶやきを編集する</h1>
         <div>
-            <a href="{{ route('tweet.index') }}">戻る</a>
+            <a href="{{ route('tweet.index') }}">< 戻る</a>
             <p>投稿フォーム</p>
+            @if (session('feedback.success'))
+                <p style="coler: green">{{ session('feedback.success') }}</p>
+            @endif
             <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
                 @method('PUT')
                 @csrf
